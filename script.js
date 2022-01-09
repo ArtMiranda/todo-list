cfa82618773d30d37b0ef4ebbf08a2f1f70c3bfb
Artur C. Miranda <artcamir12@gmail.com>
2022-01-09 02:41:27 -0300

    javascript updated, almost working, troubles on adding the second task after reloading

--- script.js
+++ script.js
@@ -1,45 +1,59 @@
 // Define the array of tasks
 let taskArray = [];
 
-// Function to check if 'enter' key was pressed
-function checkEnter(e){
-    if(e.keyCode == 13){
-        addTask();
-    }
-}
+// Event listener to check if enter key was pressed
+
+addEventListener('keypress',e=>{
+    if(e.key=="Enter"){
+
+    addTask();}
+})
+
 
 // Function that adds the written task to the array of tasks
 function addTask(){
     
+    // Define the HTML input and it's content
     let taskInput = document.getElementById("taskInput");
     let taskContent = taskInput.value;
 
+    // Adds the content of the input to the array of tasks
     taskArray.push(taskContent);
 
-    let arrayStorage = localStorage.setItem("storage", JSON.stringify(taskArray))
-    
 
+    // Get the item from the Local Storage to a array
+    let storedTask = JSON.parse(localStorage.getItem("storage"));
+
+    // Concatenates the array of tasks and the array of the Local Storage
+    let arraysConcatenated = storedTask.concat(taskArray);
+
+    // Eliminates duplicates on the array of tasks
+    const tasks = arraysConcatenated.filter((item, pos) => arraysConcatenated.indexOf(item) === pos)
+    console.log(tasks);
+
+    // Clean the text input and calls the function that prints the tasks
+    taskInput.value = "";
     refreshTasks();
 }
 
-// Function that removes all the tasks of the array of tasks and from the local storage
+// Function that removes all the tasks of the array of tasks and from the Local Storage
 function cleanTasks(){
 
+    // Cleans the array of tasks and the tasks Local Storage
     taskArray = [];
-    
     localStorage.clear();
+    tasks = [];
 
+    console.log("Tasks Cleaned");
+    // Calls the function that prints the tasks.
     refreshTasks();
 }
 
 // Function that prints the tasks on the HTML
 function refreshTasks(){
-    let storedTask = JSON.parse(localStorage.getItem("storage"));
 
-    taskArray.concat (storedTask);
-
-    console.log(taskArray);
-
+    // Set the array of tasks to be saved on the Local Storage
+    let arrayStorage = localStorage.setItem("storage", JSON.stringify(taskArray));
 
 }
 
