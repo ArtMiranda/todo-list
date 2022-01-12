222ae1ac26538fb87dd6bc958f5f2656c2a9b103
Artur C. Miranda <artcamir12@gmail.com>
2022-01-12 20:45:22 -0300

    js almost completed, still needs to work on some bugs and improvements

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 let taskArrayFromInput = [];
 let tasks = [];
 
-// Event listener to check if enter key was pressed
 addEventListener('keypress', e => {
     if (e.key == "Enter") {
 
@@ -10,26 +9,21 @@ addEventListener('keypress', e => {
     }
 })
 
-// Function that executes on the load of the page
 function onloadFunction() {
 
-    // Check if the variable storedTask exists to avoid reassignation
-    if(typeof storedTask != ""){
+    if (typeof storedTask !== "") {
         avoidBlank();
     }
-    else{
+    else {
         let arrayStorage = localStorage.setItem("storage", JSON.stringify(tasks));
     }
 }
 
-// Function that avoids blank inputs to be stored
 function avoidBlank() {
 
-    // Define the HTML input and it's content
     let taskInput = document.getElementById("taskInput");
 
-    // Avoid the add of a empty value on the string when page loaded
-    if (taskInput.value != "") {
+    if (taskInput.value !== "") {
         addTask();
     }
 
@@ -37,101 +31,76 @@ function avoidBlank() {
         tasks.pop();
         conctenateTasks();
     }
+
 }
 
-
-// Function that adds the written task to the array of tasks
 function addTask() {
 
-    // Adds the content of the input to the array of tasks
     taskArrayFromInput.push(taskInput.value);
 
-    // Calls the function that concatenate the stored tasks to the new tasks
     conctenateTasks();
+
 }
 
-// Function that concatenates the stored tasks to the new tasks 
+let storedTask = JSON.parse(localStorage.getItem("storage"));
+
 function conctenateTasks() {
 
-    // Get the item from the Local Storage to a array
-    let storedTask = JSON.parse(localStorage.getItem("storage"));
-
-    // Concatenates the array of tasks and the array of the Local Storage
     let arraysConcatenated = storedTask.concat(taskArrayFromInput);
-
-    // Eliminates duplicates on the array of tasks
     tasks = arraysConcatenated.filter((item, pos) => arraysConcatenated.indexOf(item) === pos)
-
-    // Clean the text input and calls the function that prints the tasks
     taskInput.value = "";
-
     console.log(tasks);
 
-    // Calls the function that prints the tasks on the table
     printTasks();
 
 }
 
-
-// Function that removes all the tasks of the array of tasks and from the Local Storage
 function removeTasks() {
 
-    // Cleans the array of tasks and the tasks Local Storage
     taskArrayFromInput = [];
     localStorage.clear();
+    storedTask = []; 
     tasks = [];
-
-    // Clean the text input 
-
     taskInput.value = "";
-
     alert("Tasks have been removed");
 
-    // Calls the function that prints the tasks.
     printTasks();
 }
 
-// Function that prints the tasks on the HTML table
 function printTasks() {
 
-    // Set the array of tasks to be saved on the Local Storage
     let arrayStorage = localStorage.setItem("storage", JSON.stringify(tasks));
 
 
-    // Define the table element
     let tbody = document.getElementById('tbody');
 
-    // Cleans the table before inserting content again
     tbody.innerText = "";
 
-    // Loop that gets the content of the array tasks and prints it into the table
     for (let i = 0; i < tasks.length; i++) {
         let tr = tbody.insertRow();
-
-        // Insert the rows according to the number of tasks
         let tdTask = tr.insertCell();
         let tdDelete = tr.insertCell();
 
-        // Define the content of the rows
         tdTask.innerText = tasks[i];
 
-        // Adds a class to each row of task
         tdTask.classList.add("taskContent");
 
-        // Add a image to create the delete function
         let imgDelete = document.createElement("img");
         imgDelete.src = "./images/delete.png";
+        imgDelete.addEventListener("click", e => {
+            let del = e.target.parentElement.parentElement
+            console.log(del);
+            del.remove();
 
-        // imgDelete.addEventListener("click", "test()");
+            tasks = tasks.filter(e => e !== del.innerText);
 
-        // Adds a class to the image
+            console.log(tasks);
+            console.log(storedTask);
+        });
+
         imgDelete.classList.add("deleteImage");
-
-        // Insert the image in the row of actions
         tdDelete.appendChild(imgDelete);
-    }
-}
 
-function test(){
-    alert("working");
+
+    }
 }
